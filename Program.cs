f5ce999b5718e71b9d3d1c1c0e3cba401131b247
Minister124 <kushaltmg321@gmail.com>
2025-02-06 15:01:17 +0545

    Is Development

--- Program.cs
+++ Program.cs
@@ -3,15 +3,11 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// if (!app.Environment.IsDevelopment())
-// {
-//     app.UseExceptionHandler("Home/Error");
-//     app.UseHsts();
-// }
-
-
-// app.UseHttpsRedirection(); //To redirect data from HTTP to HTTPS for encryption
-// app.UseStaticRouting(); //To enable static Routing 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("Home/Error");
+    app.UseHsts();
+}
 
 
 app.MapControllerRoute(
