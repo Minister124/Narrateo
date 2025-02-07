a66bf1dd149ebafda137ef3784b281c7f48b2779
Minister124 <kushaltmg321@gmail.com>
2025-02-07 09:07:05 +0545

    Finally

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,12 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Narrateo.Models;
 
 public class Blog
 {
-    public int Id {get; set;}
-    public string Title {get; set;}
-    public string Content {get; set;}
-    public string Author {get; set;}
+    [Required]
+    public Guid Id {get; set;}
+    public string? Title {get; set;}
+    public string? Content {get; set;}
+    public string? Author {get; set;}
     public DateTime CreatedAt {get; set;}
 }
