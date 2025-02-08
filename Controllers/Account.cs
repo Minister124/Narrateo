cf5a5944666bf3b1b714d04388528bd0531ba6fe
Minister124 <kushaltmg321@gmail.com>
2025-02-08 22:14:40 +0545

    Account

---
+++ Controllers/Account.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Narrateo.Controllers
+{
+    public class Account : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
