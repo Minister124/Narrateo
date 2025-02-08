c54127ec42ceb8034f20e29e1882c6a8ac86b7c4
Minister124 <kushaltmg321@gmail.com>
2025-02-08 21:44:07 +0545

    Model

--- Models/Login.cs
+++ Models/Login.cs
@@ -1,8 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Narrateo.Models;
 
 public class Login
 {
+    [Required]
+    public string? UserName {get; set;}
 
+    [Required]
+    public string? Password {get; set;}
 }
