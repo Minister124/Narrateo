0f4b096f15eb5b178616ba7bc96df8d711460037
Minister124 <kushaltmg321@gmail.com>
2025-02-18 15:10:46 +0545

    Restart

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Narrateo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- Program.cs
+++ Program.cs
@@ -1,31 +1,27 @@
-using Microsoft.Extensions.Options;
-using Narrateo.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("Home/Error");
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
-
 app.MapControllerRoute(
-    name: "firstRoute",
-    pattern:"{controller=Account}/{action=Login}/{id?}"
-);
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
